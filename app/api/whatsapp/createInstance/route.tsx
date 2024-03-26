b3da8005fd3faf70511b157f29cdc77af4a45c52
Lucas Santana <lucasadd15@gmail.com>
2024-03-26 15:48:44 -0300

    alter codigo

--- app/api/whatsapp/createInstance/route.tsx
+++ app/api/whatsapp/createInstance/route.tsx
@@ -1,20 +1,22 @@
-export async function GET(request: Request) {}
+import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import prisma from '@/lib/prisma';
 
-export async function HEAD(request: Request) {}
-
-export async function POST(request: Request) {
-    try {
-        const body = await request.json()
-        console.log(JSON.stringify(body))
-    } catch (error) {
-        return Response.json("error")
-        
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const user = jwt.decode(body.token)
+    if (!user) {
+      return Response.json({ message: "nao existe" }, { status: 400 });
     }
+    const createInstance = await prisma.instance.create({
+        data: {
+            
+        }
+    })
 
+    return Response.json({ message: 'teste' }, { status: 200 });
+  } catch (error) {
+    return Response.json('error');
+  }
 }
-
-export async function PUT(request: Request) {}
-
-export async function DELETE(request: Request) {}
-
-export async function PATCH(request: Request) {}
--- app/dashboard/whatsapp/page.tsx
+++ app/dashboard/whatsapp/page.tsx
@@ -1,13 +1,36 @@
-
-
-
-
+'use client';
+import Cookies from 'js-cookie';
 
 const WhatsappDasboard = () => {
+  async function CreateInstance() {
+    const token = Cookies.get('token');
+    console.log(
+      '%cMyProject%cline:6%ctoken',
+      'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
+      'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
+      'color:#fff;background:rgb(237, 222, 139);padding:3px;border-radius:2px',
+      token,
+    );
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: token,
+      }),
+    };
+    const res = await fetch(`/api/whatsapp/createInstance`, options);
+    console.log(res);
+  }
   return (
     <div className="flex flex-col m-1 bg-white text-black p-2 rounded justify-center items-center">
       <div>
-        <button className="bg-emerald-500 p-3 rounded text-white hover:bg-emerald-300">
+        <button
+          onClick={CreateInstance}
+          className="bg-emerald-500 p-3 rounded text-white hover:bg-emerald-300"
+        >
           Criar
         </button>
       </div>
